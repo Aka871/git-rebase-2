9882b710d13f68ae4b9f845a45c274c0b46b7f56
Aka871 <163530203+Aka871@users.noreply.github.com>
2024-05-31 06:40:49 +0900

    生徒名の変更

--- IdeaProjects/untitled10/src/main/java/org/example/Main.java
+++ IdeaProjects/untitled10/src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("プロジェクトを新規セットアップ!");
+    System.out.println("プロジェクトをセットアップ!");
   }
 }
